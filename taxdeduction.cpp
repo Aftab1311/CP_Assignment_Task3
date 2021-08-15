caf93c80c98c22b31f42c61aa103d0c9f0a50377
Aftab Ahmed <88955526+Aftab1311@users.noreply.github.com>
2021-08-15 17:05:29 +0530

    CPP files added and Compiled

---
+++ taxdeduction.cpp
@@ -0,0 +1,65 @@
+// Task 3
+// Name : Aftab Ahmed 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float income, tax;
+    string gender;
+    int age;
+    cout << "Enter your age : ";
+    cin >> age;
+    cout << "Enter your gender : ";
+    cin >> gender;
+    cout << "Enter your income(IN LAKHS) : ";
+    cin >> income;
+    if (income <= 2.5)
+    {
+        cout << "You don't need to pay Tax!!!!";
+    }
+    else if (income > 2.5 && income <= 5)
+    {
+        tax = 0.1 * income;
+        cout << "You need to pay your Tax amount : " << tax << " lakhs" << endl;
+    }
+    else if (income > 5 && income <= 10)
+    {
+        tax = 0.2 * income;
+        cout << "You need to pay your Tax amount : " << tax << " lakhs" << endl;
+    }
+    else if (income > 10)
+    {
+        if (gender == "male")
+        {
+            if (age >= 60)
+            {
+                tax = 0.25 * income;
+                cout << "You are a senior citizen and got benefit of 5% relaxation!!!" << endl;
+                cout << "You need to pay your Tax amount : " << tax << " lakhs" << endl;
+            }
+            else
+            {
+                tax = 0.3 * income;
+                cout << "You need to pay your Tax amount : " << tax << " lakhs" << endl;
+            }
+        }
+        else
+        {
+            if (age >= 60)
+            {
+                tax = 0.27 * income;
+                cout << "You are a woman senior citizen and got benefit of 3% relaxation!!!" << endl;
+                cout << "You need to pay your Tax amount : " << tax << " lakhs" << endl;
+            }
+            else
+            {
+                tax = 0.3 * income;
+                cout << "You need to pay your Tax amount : " << tax << " lakhs" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
