0130b7d7ae2b2f417a91d5e43fe81200074f9d7c
Aftab Ahmed <88955526+Aftab1311@users.noreply.github.com>
2021-08-15 20:06:13 +0530

    Update taxdeduction.cpp

--- taxdeduction.cpp
+++ taxdeduction.cpp
@@ -1,6 +1,16 @@
 // Task 3
 // Name : Aftab Ahmed 
 
+/* 
+AIM: WAP to calculate tax deduction as follow-
+    1. Up to 2.5L no tax.
+    2. More than 2.5 L but up to 5L 10%.
+    3. More than 5L but up to 10L 20%.
+    4. More than 10L 30%.
+        i. If senior citizen (age >= 60) then relaxation of 5%.
+        ii. if woman candidate (age >= 60) then relaxation of 3%.
+*/
+
 #include <iostream>
 using namespace std;
 
